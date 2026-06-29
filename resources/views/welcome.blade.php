296d1299ec3223d2cbef8f46eb2ff9f277d911f4
tnduy9090 <tnduy30406@gmail.com>
2026-06-29 19:21:18 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1>MSSV: 0306241268</h1>
+        <h1>Ten: Tran Ngoc Duy</h1>
+        <h1>Lop: CDCNTT 24WebD</h1>
+    </body>
+</html>
