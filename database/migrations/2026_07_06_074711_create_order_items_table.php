249fd4652815a2f710caef5d25dfc03cf9ec3168
unknown <tnduy30406@gmail.com>
2026-07-06 15:01:46 +0700

    Initial commit

---
+++ database/migrations/2026_07_06_074711_create_order_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_order',20)->unsigned();
+            $table->bigInteger('id_product',20)->unsigned();
+            $table->integer('quantity',11);
+            $table->integer('price',11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
